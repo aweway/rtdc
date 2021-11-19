0cf84bf828107d810c7c66fa060afbb392ad3d4d
Min Htet Aung <64087476+M-htet@users.noreply.github.com>
2021-11-19 23:47:37 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const 
+const exampleCheck1 = document.querySelector("#exampleCheck1");
+const exampleInputPassword1 = document.querySelector("#exampleInputPassword1");
+console.log(exampleInputPassword1)
+
+exampleCheck1.onclick = () => {
+  if( exampleInputPassword1.type === "password") {
+    exampleInputPassword1.type = "text"
+  } else {
+    exampleInputPassword1.type = "password"
+  }
+}
